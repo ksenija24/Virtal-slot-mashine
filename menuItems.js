fb2884e689258fa83738548eeb310ac6c5ec19dc
ksenija24 <94648518+ksenija24@users.noreply.github.com>
2023-01-09 17:29:50 +0100

    Add files via upload

---
+++ menuItems.js
@@ -0,0 +1,38 @@
+export const menuItems = [
+  {
+    title: 'Home',
+    url: '/',
+  },
+  {
+    title: 'Games',
+    url: '/games',
+    submenu: [
+      {
+        title: 'Game one',
+        url: 'game-one',
+      },
+      {
+        title: 'Game two',
+        url: 'game-two',
+      },
+      {
+        title: 'Game three',
+        url: 'game-three',
+      },
+    ],
+  },
+  {
+    title: 'About',
+    url: '/about',
+    submenu: [
+      {
+        title: 'Who we are',
+        url: 'who-we-are',
+      },
+      {
+        title: 'Our values',
+        url: 'our-values',
+      },
+    ],
+  },
+];
